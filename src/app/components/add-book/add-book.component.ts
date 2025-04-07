30cc704e55e026ce229173614661bddbb9d6fc4a
y2_znt <yonideserbaix@gmail.com>
2025-04-07 15:45:11 +0200

    feat: implement reactive form for adding books with validation in add-book.component.ts

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { BookService } from '../../services/book.service';
 
@@ -12,18 +17,23 @@ import { BookService } from '../../services/book.service';
 })
 export class AddBookComponent implements OnInit {
   bookForm!: FormGroup;
-  
   constructor(
     private fb: FormBuilder,
     private bookService: BookService,
     private router: Router
   ) {}
-  
   ngOnInit(): void {
     // TODO 6 : Créer un formulaire avec les champs suivants : title, author, description, category
     // TODO 7 : Ajouter les validations nécessaires
+
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      description: ['', Validators.required],
+      category: ['', Validators.required],
+    });
   }
-  
+
   onSubmit(): void {
     if (this.bookForm.valid) {
       this.bookService.addBook(this.bookForm.value).subscribe({
@@ -31,8 +41,8 @@ export class AddBookComponent implements OnInit {
           this.router.navigate(['/books']);
         },
         error: (err: any) => {
-          console.error('Erreur lors de l\'ajout du livre', err);
-        }
+          console.error("Erreur lors de l'ajout du livre", err);
+        },
       });
     }
   }
