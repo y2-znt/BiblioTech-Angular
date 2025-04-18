8391dd62b809120d09208c2dc1860a4c03acd0cd
y2_znt <yonideserbaix@gmail.com>
2025-04-18 18:16:46 +0200

    fix: move header and footer components inside the components directory

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormatTitlePipe } from '../pipes/format-title.pipe';
+import { FormatTitlePipe } from '../../pipes/format-title.pipe';
+
 @Component({
   selector: 'app-header',
   standalone: true,
