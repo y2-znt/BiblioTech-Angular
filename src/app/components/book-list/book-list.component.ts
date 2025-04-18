9438c1ea5670ef63cfbc2a52fb5366cf943f1cf0
y2_znt <yonideserbaix@gmail.com>
2025-04-18 16:58:44 +0200

    refactor: streamline book list component by renaming data to books and improving HTML structure for better readability

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BookService } from '../../services/book.service';
+import { Router, RouterLink } from '@angular/router';
 import { Book } from '../../models/book.model';
-import { Router } from '@angular/router';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -14,18 +13,14 @@ import { Router } from '@angular/router';
 })
 export class BookListComponent implements OnInit {
   books: Book[] = [];
-  data: any[] = [];
   searchTerm: string = '';
 
-  constructor(
-    private bookService: BookService,
-    private router: Router
-  ) {}
-  
+  constructor(private bookService: BookService, private router: Router) {}
+
   ngOnInit(): void {
     this.loadBooks();
   }
-  
+
   loadBooks(): void {
     this.bookService.getBooks().subscribe({
       next: (books: Book[]) => {
@@ -33,10 +28,10 @@ export class BookListComponent implements OnInit {
       },
       error: (err: any) => {
         console.error('Erreur lors du chargement des livres:', err);
-      }
+      },
     });
   }
-  
+
   toggleFavorite(book: Book): void {
     this.bookService.toggleFavorite(book.id).subscribe({
       next: (updatedBook: Book) => {
@@ -45,10 +40,10 @@ export class BookListComponent implements OnInit {
       error: (err: any) => {
         // TODO 17: Affiche une alerte qui indique que la modification du favori a échoué
         console.error('Erreur lors de la modification du favori:', err);
-      }
+      },
     });
   }
-  
+
   deleteBook(id: string): void {
     this.bookService.deleteBook(id).subscribe({
       next: () => {
@@ -58,9 +53,9 @@ export class BookListComponent implements OnInit {
       error: (err: any) => {
         // TODO 19: Affiche une alerte qui indique que la suppression du livre a échoué
         console.error('Erreur lors de la suppression du livre:', err);
-      }
+      },
     });
-  } 
+  }
 
   goToBookDetails(id: string): void {
     this.router.navigate(['/books', id]);
